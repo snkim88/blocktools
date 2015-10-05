e968521a926e2244579eb0bbe3414a97b724290c
Hendrik 'Bloody' Schumann <bloody@fluffl.es>
2015-10-05 17:42:36 +0200

    Pythonify:     - Tabs to spaces     - rewrite toString() to __unicode__()     - Fix README markdown

--- block.py
+++ block.py
@@ -1,100 +1,133 @@
-from blocktools import *
+from blocktools import * 
 
 class BlockHeader:
-	def __init__(self, blockchain):
-		self.version = uint4(blockchain)
-		self.previousHash = hash32(blockchain)
-		self.merkleHash = hash32(blockchain)
-		self.time = uint4(blockchain)
-		self.bits = uint4(blockchain)
-		self.nonce = uint4(blockchain)
-	def toString(self):
-		print "Version:\t %d" % self.version
-		print "Previous Hash\t %s" % hashStr(self.previousHash)
-		print "Merkle Root\t %s" % hashStr(self.merkleHash)
-		print "Time\t\t %s" % str(self.time)
-		print "Difficulty\t %8x" % self.bits
-		print "Nonce\t\t %s" % self.nonce
+    def __init__(self, blockchain):
+        self.version = uint4(blockchain)
+        self.previousHash = hash32(blockchain)
+        self.merkleHash = hash32(blockchain)
+        self.time = uint4(blockchain)
+        self.bits = uint4(blockchain)
+        self.nonce = uint4(blockchain)
+        
+    def __unicode__(self):
+        return """Version:\t %d
+Previous Hash\t %s 
+Merkle Root\t %s" 
+Time\t\t %s" 
+Difficulty\t %8x
+Nonce\t\t %s""" % (
+                    self.version, hashStr(self.previousHash), hashStr(self.merkleHash),
+                    str(self.time), self.bits, self.nonce
+                )
+                
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
 
 class Block:
-	def __init__(self, blockchain):
-		self.magicNum = uint4(blockchain)
-		self.blocksize = uint4(blockchain)
-		self.setHeader(blockchain)
-		self.txCount = varint(blockchain)
-		self.Txs = []
+    def __init__(self, blockchain):
+        self.magicNum = uint4(blockchain)
+        self.blocksize = uint4(blockchain)
+        self.setHeader(blockchain)
+        self.txCount = varint(blockchain)
+        self.Txs = []
 
-		for i in range(0, self.txCount):
-			tx = Tx(blockchain)
-			self.Txs.append(tx)
+        for i in range(0, self.txCount):
+            tx = Tx(blockchain)
+            self.Txs.append(tx)
 
-	def setHeader(self, blockchain):
-		self.blockHeader = BlockHeader(blockchain)
-
-	def toString(self):
-		print ""
-		print "Magic No: \t%8x" % self.magicNum
-		print "Blocksize: \t", self.blocksize
-		print ""
-		print "#"*10 + " Block Header " + "#"*10
-		self.blockHeader.toString()
-		print 
-		print "##### Tx Count: %d" % self.txCount
-		for t in self.Txs:
-			t.toString()
+    def setHeader(self, blockchain):
+        self.blockHeader = BlockHeader(blockchain)
+        
+    def __unicode__(self):
+            return """Magic No: \t%8x
+Blocksize: \t%d
+########## Block Header ##########
+%s
+##### Tx Count: %d
+%s
+"""         % (
+                self.magicNum, self.blocksize, str(self.blockHeader), self.txCount,
+                '\n'.join([str(tx) for tx in self.Txs])
+            )
+            
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
 
 class Tx:
-	def __init__(self, blockchain):
-		self.version = uint4(blockchain)
-		self.inCount = varint(blockchain)
-		self.inputs = []
-		for i in range(0, self.inCount):
-			input = txInput(blockchain)
-			self.inputs.append(input)
-		self.outCount = varint(blockchain)
-		self.outputs = []
-		if self.outCount > 0:
-			for i in range(0, self.outCount):
-				output = txOutput(blockchain)
-				self.outputs.append(output)	
-		self.lockTime = uint4(blockchain)
-		
-	def toString(self):
-		print ""
-		print "="*10 + " New Transaction " + "="*10
-		print "Tx Version:\t %d" % self.version
-		print "Inputs:\t\t %d" % self.inCount
-		for i in self.inputs:
-			i.toString()
-
-		print "Outputs:\t %d" % self.outCount
-		for o in self.outputs:
-			o.toString()
-		print "Lock Time:\t %d" % self.lockTime
-				
+    def __init__(self, blockchain):
+        self.version = uint4(blockchain)
+        self.inCount = varint(blockchain)
+        self.inputs = []
+        for i in range(0, self.inCount):
+            input = txInput(blockchain)
+            self.inputs.append(input)
+        self.outCount = varint(blockchain)
+        self.outputs = []
+        if self.outCount > 0:
+            for i in range(0, self.outCount):
+                output = txOutput(blockchain)
+                self.outputs.append(output)    
+        self.lockTime = uint4(blockchain)
+        
+    def __unicode__(self):
+        return """========== New Transaction ==========
+Tx Version: \t%d
+Inputs: \t%d
+%s
+Outputs: \t%d
+%s
+Lock Time: \t%d
+"""     % (
+            self.version,
+            self.inCount, '\n'.join([str(i) for i in self.inputs]),
+            self.outCount, '\n'.join([str(o) for o in self.outputs]),
+            self.lockTime
+        )
+            
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
+                
 
 class txInput:
-	def __init__(self, blockchain):
-		self.prevhash = hash32(blockchain)
-		self.txOutId = uint4(blockchain)
-		self.scriptLen = varint(blockchain)
-		self.scriptSig = blockchain.read(self.scriptLen)
-		self.seqNo = uint4(blockchain)
-
-	def toString(self):
-		print "Previous Hash:\t %s" % hashStr(self.prevhash)
-		print "Tx Out Index:\t %8x" % self.txOutId
-		print "Script Length:\t %d" % self.scriptLen
-		print "Script Sig:\t %s" % hashStr(self.scriptSig)
-		print "Sequence:\t %8x" % self.seqNo
-		
+    def __init__(self, blockchain):
+        self.prevhash = hash32(blockchain)
+        self.txOutId = uint4(blockchain)
+        self.scriptLen = varint(blockchain)
+        self.scriptSig = blockchain.read(self.scriptLen)
+        self.seqNo = uint4(blockchain)
+        
+    def __unicode__(self):
+        return """< Previous Hash: \t%s
+< Tx Out Index: \t%8x
+< Script Length: \t%d
+< Script Sig: \t\t%s
+< Sequence: \t\t%8x
+"""     % (
+            hashStr(self.prevhash),
+            self.txOutId,
+            self.scriptLen,
+            hashStr(self.scriptSig),
+            self.seqNo
+        )
+            
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
+        
 class txOutput:
-	def __init__(self, blockchain):	
-		self.value = uint8(blockchain)
-		self.scriptLen = varint(blockchain)
-		self.pubkey = blockchain.read(self.scriptLen)
-
-	def toString(self):
-		print "Value:\t\t %d" % self.value
-		print "Script Len:\t %d" % self.scriptLen
-		print "Pubkey:\t\t %s" % hashStr(self.pubkey)
+    def __init__(self, blockchain):    
+        self.value = uint8(blockchain)
+        self.scriptLen = varint(blockchain)
+        self.pubkey = blockchain.read(self.scriptLen)
+        
+    def __unicode__(self):
+        return """> Value: \t%d
+> Script Len: \t%d
+> Pubkey: \t%s
+"""     % (
+            self.value,
+            self.scriptLen,
+            hashStr(self.pubkey)
+        )
+            
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
--- sight.py
+++ sight.py
@@ -4,22 +4,27 @@ from blocktools import *
 from block import Block, BlockHeader
 
 def parse(blockchain):
-	print 'print Parsing Block Chain'
-	counter = 0
-	while True:
-		print counter
-		block = Block(blockchain)
-		block.toString()
-		counter+=1
+    print 'Parsing Block Chain'
+    counter = 0
+    while True:
+        print counter
+        try:
+            block = Block(blockchain)
+            print str(block)
+        except Exception as ex:
+            print 'Error:', ex
+            break
+            
+        counter+=1
 
 def main():
-	if len(sys.argv) < 2:
+    if len(sys.argv) < 2:
             print 'Usage: sight.py filename'
-	else:
-		with open(sys.argv[1], 'rb') as blockchain:
-			parse(blockchain)
+    else:
+        with open(sys.argv[1], 'rb') as blockchain:
+            parse(blockchain)
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
